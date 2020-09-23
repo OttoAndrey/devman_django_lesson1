f798f90ed10298d6263ad03a1f1013ef52f57013
Ray <ottotruepro@gmail.com>
2020-09-23 09:06:05 +0700

    Hide SECRET_KEY

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nh7l=-93udoi*b8rfr@l2btq6n^24c3-6thb%^k#ibuf6@*=qh'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!

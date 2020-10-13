cb46501ec01962298a2161e74ac88762f05f66fa
Ray <ottotruepro@gmail.com>
2020-10-13 22:40:37 +0700

    Default values in settings.py

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -27,13 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-
+SECRET_KEY = os.getenv('SECRET_KEY') or 'some-secret-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default='127.0.0.1')
 
 # Application definition
 
